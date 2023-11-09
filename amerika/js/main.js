3ad26812f14168b5ffe5b9addf954d77e7dfa342
Leon Westerveld <35258@ma-web.nl>
2023-11-09 12:52:00 +0100

    js

--- amerika/js/main.js
+++ amerika/js/main.js
@@ -1,8 +1,7 @@
-// let star = document.querySelector('.star__cage');
-// console.log(star);
-// for(let x = 0; x < 50; x++) {
-// 	let cln = star.cloneNode(true);
-// 	document.body.append(cln)
-// }
+let star = document.querySelector('.star__cage');
+let blauw = star.parentElement;
 
-// dit is een code om de stars te clone ipv zoveel in de html te zetten, maar is gevaalt
+for (var i = 0; i < 51; i++) {
+  var clonedDiv = star.cloneNode(true); 
+  blauw.appendChild(clonedDiv);
+}
