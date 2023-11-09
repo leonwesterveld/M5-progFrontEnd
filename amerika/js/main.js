743ca42167ca903eeb08229cc5ac67856d76c614
Leon Westerveld <35258@ma-web.nl>
2023-11-09 12:53:14 +0100

    js

--- amerika/js/main.js
+++ amerika/js/main.js
@@ -5,3 +5,5 @@ for (var i = 0; i < 51; i++) {
   var clonedDiv = star.cloneNode(true); 
   blauw.appendChild(clonedDiv);
 }
+
+// dit is er om de div '.star__cage' 50 keer te clone ziet er beter ui in de html
