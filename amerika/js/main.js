a15fe167587c988869757416b43b843924c3b06d
Leon Westerveld <35258@ma-web.nl>
2023-11-09 02:45:47 +0100

    amerika

---
+++ amerika/js/main.js
@@ -0,0 +1,6 @@
+// let star = document.querySelector('.star__cage');
+// console.log(star);
+// for(let x = 0; x < 50; x++) {
+// 	let cln = star.cloneNode(true);
+// 	document.body.append(cln)
+// }
